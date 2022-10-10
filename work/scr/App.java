2bf003e43602521adcbb4694d70e6d9fec7d6db7
Kochukov Aleksei <94113429+sayman88@users.noreply.github.com>
2022-10-10 23:05:18 +0500

    Update and rename work2 to work/scr/App.java

---
+++ work/scr/App.java
@@ -0,0 +1,14 @@
+
+package ru.iq_soft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
